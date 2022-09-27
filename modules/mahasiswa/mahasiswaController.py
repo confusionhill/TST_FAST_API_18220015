7a0ad536b48347d17853531daaa9edb63bc4555b
farhan <18220015@std.stei.itb.ac.id>
2022-09-27 17:56:46 +0700

    added simple exception

--- modules/mahasiswa/mahasiswaController.py
+++ modules/mahasiswa/mahasiswaController.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from models.mahasiswa import Mahasiswa
 
@@ -8,6 +8,8 @@ mahasiswaRouter = APIRouter(
 
 @mahasiswaRouter.get("/")
 async def getMahasiswa(nim: int, nama: str):
+    if nim < 10000000 or nim > 19999999:
+        raise HTTPException(status_code=404, detail="NIM is not available")
     dataMahasiswa = {
         "nim": nim,
         "nama": nama.replace('"',"")
@@ -16,4 +18,6 @@ async def getMahasiswa(nim: int, nama: str):
 
 @mahasiswaRouter.post("/")
 async def postMahasiswa(mahasiswa: Mahasiswa):
+    if ((mahasiswa.nim < 10000000) or (mahasiswa.nim > 19999999) ):
+        raise HTTPException(status_code=404, detail="NIM is not available")
     return mahasiswa
